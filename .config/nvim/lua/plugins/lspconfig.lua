621bbcfd183a8bc28e6d44926b4657b727c49737
up-the-hill <aj.sth.2022@gmail.com>
2025-05-18 15:03:18 +1000

    nvim updates, and fixed tmux-sessionizer for folders with spaces

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,12 +1,12 @@
+-- Guide to configuring lsp configs:
+-- https://github.com/neovim/nvim-lspconfig/tree/master/lua/lspconfig/configs
+local lspconfig = require("lspconfig")
+local util = require("lspconfig.util")
+
 return {
   "neovim/nvim-lspconfig",
 
-  -- dependencies = {
-  --   "williamboman/mason.nvim",
-  -- },
-
   config = function()
-    local lspconfig = require("lspconfig")
     lspconfig.emmet_language_server.setup({
       filetypes = {
         "css",
@@ -21,29 +21,31 @@ return {
         "typescriptreact",
         "astro",
       },
-      -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-      -- **Note:** only the options listed in the table are supported.
-      init_options = {
-        ---@type table<string, string>
-        includeLanguages = {},
-        --- @type string[]
-        excludeLanguages = {},
-        --- @type string[]
-        extensionsPath = {},
-        --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-        preferences = {},
-        --- @type boolean Defaults to `true`
-        showAbbreviationSuggestions = true,
-        --- @type "always" | "never" Defaults to `"always"`
-        showExpandedAbbreviation = "always",
-        --- @type boolean Defaults to `false`
-        showSuggestionsAsSnippets = false,
-        --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-        syntaxProfiles = {},
-        --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-        variables = {},
+    })
+
+    lspconfig.cssls.setup({
+      filetypes = {
+        "css",
+        "scss",
+        "less",
+        "astro",
       },
     })
-    -- ...
+
+    lspconfig.ts_ls.setup({
+      capabilities = capabilities,
+      init_options = { hostInfo = "neovim" },
+      cmd = { "typescript-language-server", "--stdio" },
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+      },
+      root_dir = util.root_pattern("tsconfig.json", "jsconfig.json", "package.json", ".git"),
+      single_file_support = true,
+    })
   end,
 }
