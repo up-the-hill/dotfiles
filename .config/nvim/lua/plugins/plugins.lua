9478da34795280e826c95342a784aca24b2db0b2
up-the-hill <aj.sth.2022@gmail.com>
2025-07-12 09:34:51 +1000

    lazyvim update

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -14,4 +14,12 @@ return {
   {
     "tridactyl/vim-tridactyl",
   },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
